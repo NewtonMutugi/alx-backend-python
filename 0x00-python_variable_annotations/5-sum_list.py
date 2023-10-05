a74fb5a30e3a52ef41c90c1f309fe93f78d7b81e
NewtonMutugi <newtonmut197@gmail.com>
2023-10-06 02:36:41 +0300

    Modified 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """type-annotated function"""
+from typing import List
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """takes a list input_list of floats as argument and returns their sum"""
-    return sum(input_list)
+    return float(sum(input_list))

05b8125ff23beb2467d6a15ff0d892606ac4de01
NewtonMutugi <newtonmut197@gmail.com>
2023-10-30 23:10:50 +0300

    Added required files#3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -26,3 +26,40 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as error:
             access_nested_map(nested_map, path)
         self.assertEqual(f"KeyError('{expected}')", repr(error.exception))
+
+
+class TestGetJson(unittest.TestCase):
+    """ TestGetJson """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ test_get_json """
+        mock = Mock()
+        mock.json.return_value = test_payload
+        with patch('requests.get', return_value=mock):
+            self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ TestMemoize """
+
+    def test_memoize(self):
+        """ test_memoize """
+        class TestClass:
+            """ TestClass """
+
+            def a_method(self):
+                """ a_method """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ a_property """
+                return self.a_method()
+        with patch.object(TestClass, 'a_method', return_value=42) as mock:
+            test = TestClass()
+            test.a_property
+            test.a_property
+            mock.assert_called_once()
